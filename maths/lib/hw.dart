eb113aaee6859c1edc19f8a8018b09a0817bccb8
SightedBike273 <sighted@kincode.net>
2023-02-25 18:33:37 +0000

    homework tab

--- maths/lib/hw.dart
+++ maths/lib/hw.dart
@@ -6,6 +6,76 @@ class HwPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Homework")),
+      body: Column(children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            "Table",
+            textScaleFactor: 2,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Table(
+            textDirection: TextDirection.ltr,
+            defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
+            border: TableBorder.symmetric(inside: const BorderSide(width: 0.1)),
+            children: [
+              const TableRow(children: [
+                Text(
+                  "Task",
+                  textScaleFactor: 1.5,
+                ),
+                Text("Teacher", textScaleFactor: 1.5),
+                Text(" ", textScaleFactor: 1.5),
+              ]),
+              TableRow(children: [
+                const Text("Basic Addition", textScaleFactor: 1.5),
+                const Text("Mr Brooks", textScaleFactor: 1.5),
+                ButtonBar(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        throw ("Not Implemented");
+                      },
+                      child: const Text("Complete"),
+                    ),
+                  ],
+                ),
+              ]),
+              TableRow(children: [
+                const Text("Basic Subtraction", textScaleFactor: 1.5),
+                const Text("Mr George", textScaleFactor: 1.5),
+                ButtonBar(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        throw ("Not Implemented");
+                      },
+                      child: const Text("Complete"),
+                    ),
+                  ],
+                ),
+              ]),
+              TableRow(children: [
+                const Text("Basic Multiplication", textScaleFactor: 1.5),
+                const Text("Mrs Kinney", textScaleFactor: 1.5),
+                ButtonBar(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        throw ("Not Implemented");
+                      },
+                      child: const Text("Complete"),
+                    ),
+                  ],
+                ),
+              ]),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
