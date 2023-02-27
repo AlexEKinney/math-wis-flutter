c4d552a0fcb9e9110c5e646ac326386c57449e3c
SightedBike273 <sighted@kincode.net>
2023-02-27 20:23:32 +0000

    changes

--- maths/lib/login.dart
+++ maths/lib/login.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -110,7 +113,9 @@ class __FormContentState extends State<_FormContent> {
                     await service.signInwithGoogle();
                     Navigator.pushReplacementNamed(context, "/dash");
                   } catch (e) {
-                    print(e);
+                    if (kDebugMode) {
+                      print(e);
+                    }
                   }
                 },
               ),
